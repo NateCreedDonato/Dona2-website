2b74ae525f64f13a61e39f6126d4ebf2fa9b7a46
Nathaniel Donato <nathanieldonatofx@gmail.com>
2024-04-10 22:16:02 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -29,7 +29,6 @@ get_header();
 			<p>Do you ever care at all about how many hearts you break</p>
 			<p>I care way too much and honestly its my biggest mistate</p>
 			<p>You probabaly think that every song is about you but its not</p>
-			<p>Yeah Im guilty of it too but its the last one Ive got</p>
 		
 		</div><!-- end caption -->
 		
